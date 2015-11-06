14d0caebde7eda62376b15634a9bdf7a900642d5
nancytao <nancy.tao42@gmail.com>
2015-11-06 14:26:53 -0500

    Added to Problem 1

---
+++ Problem01.java
@@ -0,0 +1,22 @@
+/**
+ * Find the sum of all the multiples of 3 and 5 below 1000
+ * @author Nancy Tao
+ * @version 11/06/2015
+ */
+public class Problem01 {
+    public static void main(String[] args) {
+        System.out.println(new Problem01().sum(1000));
+    }
+
+    public int sum(int upperLimit) {
+        int toReturn = 0;
+
+        for (int i = 0; i < upperLimit; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                toReturn += i;
+            }
+        }
+
+        return toReturn;
+    }
+}
