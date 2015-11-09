22aa40de3e522622257c9c2f64a84a30111dd50b
nancytao <nancy.tao42@gmail.com>
2015-11-09 10:57:45 -0500

    Added solution to problem 7

---
+++ Problem07.java
@@ -0,0 +1,32 @@
+/**
+ * What is the 10 001st prime number?
+ * @author Nancy Tao
+ * @version 11/09/2015
+ */
+public class Problem07 {
+    public static void main(String[] args) {
+        System.out.println(new Problem07().nthPrime(10001));
+    }
+
+    public int nthPrime(int n) {
+        int counter = 0;
+        for (int i = 2; ; i++) {
+            if (isPrime(i)) {
+                counter++;
+                if (counter == n) {
+                    return i;
+                }
+            }
+        }
+    }
+
+    public boolean isPrime(int num) {
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
