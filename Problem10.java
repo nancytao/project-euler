ed8c6af22d31d8850c8d846e1a1e8c4178c35369
nancytao <nancy.tao42@gmail.com>
2015-12-17 20:37:22 -0500

    Made prime generator a static nested class

--- Problem10.java
+++ Problem10.java
@@ -5,7 +5,7 @@ import java.util.List;
  * Find the sum of all the primes below two million.
  *
  * @author Nancy Tao
- * @version 12/11/2015
+ * @version 12/17/2015
  */
 public class Problem10 {
     public static void main(String[] args) {
