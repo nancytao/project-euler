05f7e9a97d5285d61a59cac874fad59bd415d268
nancytao <nancy.tao42@gmail.com>
2015-12-22 19:45:33 -0500

    Added javadocs

--- Problem48.java
+++ Problem48.java
@@ -1,5 +1,12 @@
 import java.math.BigInteger;
 
+/**
+ * The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317. Find the last ten digits of the series,
+ * 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+ *
+ * @author Nancy Tao
+ * @version 12/22/2015
+ */
 public class Problem48 {
     public static void main(String[] args) {
         BigInteger sum = BigInteger.ZERO;
